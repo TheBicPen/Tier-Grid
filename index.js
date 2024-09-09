c69822dab3a3ffc670387d3cd97603c6d1783d3d
Alex <mashkal2000@gmail.com>
2024-09-08 23:54:55 -0700

    Add background colors to cells

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 "use strict"
 
+const BASE_COLOR = [53, 53, 53];
+const AXIS_X_COLOR = [100, 110, 10];
+const AXIS_Y_COLOR = [10, 60, 130];
+
+function rgb_to_css(r, g, b) {
+    return `rgb(${[r, g, b].join(', ')})`;
+}
+
+function int_lerp(start, end, count) {
+    const diff = end - start;
+    const inc = diff / count;
+    return range(count).map(
+        (_, index) => Math.round(start + inc * index)
+    );
+}
+
+function range(end) {
+    return Array.from(Array(end).keys());
+}
+
+function combine_colors(a, b) {
+    // Really should use a better color space than RGB
+    // With colors that overlap, this produces more vivid colors than arithmetic mean
+    return range(3).map((_, index) => Math.round(Math.sqrt(a[index] * b[index])));
+}
+
+function set_table_cell_color(table) {
+    const y = table.rows.length;
+    const x = table.rows[y - 1].cells.length;
+    const colors_x_r = int_lerp(BASE_COLOR[0], AXIS_X_COLOR[0], x);
+    const colors_x_g = int_lerp(BASE_COLOR[1], AXIS_X_COLOR[1], x);
+    const colors_x_b = int_lerp(BASE_COLOR[2], AXIS_X_COLOR[2], x);
+    const colors_y_r = int_lerp(BASE_COLOR[0], AXIS_Y_COLOR[0], y);
+    const colors_y_g = int_lerp(BASE_COLOR[1], AXIS_Y_COLOR[1], y);
+    const colors_y_b = int_lerp(BASE_COLOR[2], AXIS_Y_COLOR[2], y);
+    const colors_x_rgb = range(x).map(
+        (_, index) => [colors_x_r[index], colors_x_g[index], colors_x_b[index]]
+    );
+    const colors_y_rgb = range(y).map(
+        (_, index) => [colors_y_r[index], colors_y_g[index], colors_y_b[index]]
+    );
+    for (const [i, row] of Array.from(table.rows).entries()) {
+        for (const [j, cell] of Array.from(row.cells).entries()) {
+            const cell_color_rgb = combine_colors(colors_x_rgb[j], colors_y_rgb[y - i - 1]);
+            cell.style.backgroundColor = rgb_to_css(...cell_color_rgb);
+        }
+    }
+}
+
 function fill_table(table, x, y) {
     for (let i = 0; i < y; i++) {
         const row = document.createElement("tr");
@@ -46,5 +95,7 @@ const tier_grid = document.getElementById("tier-grid");
 const image_bank = document.getElementById("image-bank");
 const image_bank_label = document.getElementById("image-bank-label");
 fill_table(tier_grid, 10, 10);
-const containers = Array.from(tier_grid.rows).flatMap(row => Array.from(row.cells).map(table_cell => table_cell.children[0])).concat([image_bank]);
+set_table_cell_color(tier_grid);
+const containers = Array.from(tier_grid.rows).flatMap(
+    row => Array.from(row.cells).map(table_cell => table_cell.children[0])).concat([image_bank]);
 const drake = dragula(containers);
