5a9d8abfe3ecc11ef71522b11294defa9b02fe0d
Alex <mashkal2000@gmail.com>
2024-09-14 21:46:48 -0700

    Allow expanding grid
    Refactor the tier grid into a class to keep the state in sync

--- index.js
+++ index.js
@@ -1,113 +1,155 @@
 "use strict"
 
-const AXIS_X_COLOR_ADDITION_OKLCH = [0, 0.1, 330];
-const AXIS_Y_COLOR_ADDITION_OKLCH = [0, 0.1, 170];
-const HUE_MIDPOINT = (AXIS_X_COLOR_ADDITION_OKLCH[2] + AXIS_Y_COLOR_ADDITION_OKLCH[2]) / 2;
+class TierGrid {
+    #tier_grid;
+    #color_x_layer;
+    #color_y_layer;
+    #image_bank;
+    #image_bank_label;
 
-function add_oklch_color(color_var_name, components, a) {
-    return `oklch(from var(--${color_var_name}) calc(l + ${components[0]}) calc(c + ${components[1]}) calc(h + ${components[2]}) / ${a})`;
-}
+    static #BASE_COLOR_CSS_NAME = "base-color";
 
-function color_combiner(hue_midpt) {
-    return function combine_colors(x, y) {
-        // We don't want the lightness to be more than the sum of its parts
-        // Raising chroma to a higher power creates a nicer gradient
-        // Take the average hue
+    constructor(tier_grid_element, init_x, init_y, color_x_layer, color_y_layer, image_bank, image_bank_label) {
+        this.#tier_grid = tier_grid_element;
+        this.#color_x_layer = color_x_layer;
+        this.#color_y_layer = color_y_layer;
+        this.#image_bank = image_bank;
+        this.#image_bank_label = image_bank_label;
 
+        // Fill grid cells per initial dimensions
+        for (let i = 0; i < init_y; i++) {
+            const row = document.createElement("tr");
+            for (let j = 0; j < init_x; j++) {
+                TierGrid.#add_cell(row);
+            }
+            this.#tier_grid.appendChild(row);
+        }
+        this.#update_tier_grid();
+    }
+
+    // Make cells draggable container and set their color
+    #update_tier_grid() {
+        TierGrid.#set_table_cell_color(this.#tier_grid, this.#color_x_layer, this.#color_y_layer);
+        const containers = Array.from(this.#tier_grid.rows).flatMap(
+            row => Array.from(row.cells).map(table_cell => table_cell.children[0])).concat([this.#image_bank]);
+        dragula(containers);
+    }
+
+    static #add_oklch_color(components, a) {
+        return `oklch(from var(--${TierGrid.#BASE_COLOR_CSS_NAME}) calc(l + ${components[0]}) calc(c + ${components[1]}) calc(h + ${components[2]}) / ${a})`;
+    }
+
+    static #combine_colors(x, y, hue_midpt) {
         return [
-            Math.max(x[0], y[0]),       // L
-            Math.max(x[1], y[1]),       // C
-            x[2] - y[2] + hue_midpt     // H
+            // We don't want the lightness to be more than the sum of its parts
+            Math.max(x[0], y[0]),               // L
+            Math.max(x[1], y[1]),               // C
+            // Take the average hue
+            x[2] - y[2] + hue_midpt             // H
         ];
     }
-}
 
-function set_cell_opacity(table, opacity) {
-    for (const row of table.rows) {
-        for (const cell of row.cells) {
-            const current_background_color = cell.style.backgroundColor;
-            const components = Array.from(current_background_color.matchAll(/[\d.]+/g));
-            console.assert(components.length == 4);
-            const opacity_match = components[3];
-            cell.style.backgroundColor = current_background_color.slice(0, opacity_match.index) + opacity + current_background_color.slice(opacity_match.index + opacity_match.length);
+    static #set_table_cell_color(table, x_color, y_color) {
+        const hue_midpt = (x_color[2] + y_color[2]) / 2;
+        const y = table.rows.length;
+        const x = table.rows[y - 1].cells.length;
+        for (const [i, row] of Array.from(table.rows).entries()) {
+            for (const [j, cell] of Array.from(row.cells).entries()) {
+                const colors_x_lch = [
+                    x_color[0] * j / x,
+                    x_color[1] * j / x,
+                    // lerp from H_X to HUE_MIDPT, offset by -HUE_MIDPT
+                    (hue_midpt - x_color[2]) * j / x,
+                ];
+                const colors_y_lch = [
+                    y_color[0] * (y - i - 1) / y,
+                    y_color[1] * (y - i - 1) / y,
+                    // lerp from HUE_MIDPT to Y_H, offset by -HUE_MIDPT
+                    (y_color[2] - hue_midpt) * (y - i - 1) / y,
+                ];
+                const cell_color = TierGrid.#combine_colors(colors_x_lch, colors_y_lch, hue_midpt);
+                const new_color = TierGrid.#add_oklch_color(cell_color, 1);
+                cell.style.backgroundColor = new_color;
+                // for debugging
+                // cell.innerText = String(cell_color[2]).slice(0, 4);
+            }
         }
     }
-}
 
-function set_table_cell_color(table) {
-    const y = table.rows.length;
-    const x = table.rows[y - 1].cells.length;
-    const combine_colors = color_combiner(HUE_MIDPOINT);
-    for (const [i, row] of Array.from(table.rows).entries()) {
-        for (const [j, cell] of Array.from(row.cells).entries()) {
-            const colors_x_lch = [
-                AXIS_X_COLOR_ADDITION_OKLCH[0] * j / x,
-                AXIS_X_COLOR_ADDITION_OKLCH[1] * j / x,
-                // lerp from H_X to HUE_MIDPT, offset by -HUE_MIDPT
-                (HUE_MIDPOINT - AXIS_X_COLOR_ADDITION_OKLCH[2]) * j / x,
-            ];
-            const colors_y_lch = [
-                AXIS_Y_COLOR_ADDITION_OKLCH[0] * (y - i - 1) / y,
-                AXIS_Y_COLOR_ADDITION_OKLCH[1] * (y - i - 1) / y,
-                // lerp from HUE_MIDPT to Y_H, offset by -HUE_MIDPT
-                (AXIS_Y_COLOR_ADDITION_OKLCH[2] - HUE_MIDPOINT) * (y - i - 1) / y,
-            ];
-            const cell_color = combine_colors(colors_x_lch, colors_y_lch);
-            const new_color = add_oklch_color("base-color", cell_color, 1);
-            cell.style.backgroundColor = new_color;
-            // for debugging
-            // cell.innerText = String(cell_color[2]).slice(0, 4);
+    static #add_cell(row) {
+        const table_cell = document.createElement("td");
+        const cell = document.createElement("div");
+        cell.className = "table-cell";
+        table_cell.appendChild(cell);
+        row.appendChild(table_cell);
+    }
+
+    set_cell_opacity(opacity) {
+        for (const row of this.#tier_grid.rows) {
+            for (const cell of row.cells) {
+                const current_background_color = cell.style.backgroundColor;
+                const components = Array.from(current_background_color.matchAll(/[\d.]+/g));
+                console.assert(components.length == 4);
+                const opacity_match = components[3];
+                cell.style.backgroundColor = current_background_color.slice(0, opacity_match.index)
+                    + opacity
+                    + current_background_color.slice(opacity_match.index + opacity_match.length);
+            }
         }
     }
-}
 
-function fill_table(table, x, y) {
-    for (let i = 0; i < y; i++) {
+    add_row() {
+        const x = this.#tier_grid.rows[0].cells.length;
         const row = document.createElement("tr");
-        for (let j = 0; j < x; j++) {
-            const table_cell = document.createElement("td");
-            const cell = document.createElement("div");
-            cell.className = "table-cell";
-            table_cell.appendChild(cell);
-            row.appendChild(table_cell);
+        this.#tier_grid.appendChild(row);
+        for (let i = 0; i < x; i++) {
+            TierGrid.#add_cell(row);
         }
-        table.appendChild(row);
+        this.#update_tier_grid();
+    }
+
+    add_col() {
+        for (const row of Array.from(this.#tier_grid.rows)) {
+            TierGrid.#add_cell(row);
+        }
+        this.#update_tier_grid();
+    }
+
+    add_to_drag_zone(ev) {
+        // Prevent default behavior (Prevent file from being opened)
+        ev.preventDefault();
+        const dt = ev.dataTransfer;
+        const files = dt.files;
+
+        for (const file of files) {
+            if (!file.type.startsWith("image/")) {
+                continue;
+            }
+
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const img = new Image();
+                img.src = e.target.result;
+                this.#image_bank_label.hidden = true;
+                this.#image_bank.appendChild(img);
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+
+    static drag_zone_prevent_default_drag(ev) {
+        // Prevent default behavior (Prevent file from being opened)
+        ev.preventDefault();
     }
 }
 
-function add_to_drag_zone(ev) {
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-    const dt = ev.dataTransfer;
-    const files = dt.files;
 
-    for (const file of files) {
-        if (!file.type.startsWith("image/")) {
-            continue;
-        }
-
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            const img = new Image();
-            img.src = e.target.result;
-            image_bank_label.hidden = true;
-            image_bank.appendChild(img);
-        };
-        reader.readAsDataURL(file);
-    }
-}
-
-
-function drag_zone_prevent_default_drag(ev) {
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-}
-
-const tier_grid = document.getElementById("tier-grid");
-const image_bank = document.getElementById("image-bank");
-const image_bank_label = document.getElementById("image-bank-label");
-fill_table(tier_grid, 7, 7);
-set_table_cell_color(tier_grid);
-const containers = Array.from(tier_grid.rows).flatMap(
-    row => Array.from(row.cells).map(table_cell => table_cell.children[0])).concat([image_bank]);
-const drake = dragula(containers);
+const AXIS_X_COLOR_OKLCH = [0, 0.1, 330];
+const AXIS_Y_COLOR_OKLCH = [0, 0.1, 170];
+const TIER_GRID = new TierGrid(
+    document.getElementById("tier-grid"),
+    7, 7,
+    AXIS_X_COLOR_OKLCH, AXIS_Y_COLOR_OKLCH,
+    document.getElementById("image-bank"),
+    document.getElementById("image-bank-label")
+);
